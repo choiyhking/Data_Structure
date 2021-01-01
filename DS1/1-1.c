5335438b284ca799ea11499c6d345fb71ba793e6
최윤하 <choiyhking@gmail.com>
2021-01-01 10:34:01 +0000

    source code of 1-1

---
+++ DS1/1-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1, num2;
+
+	printf("Two Numbers: ");
+	scanf("%d %d", &num1, &num2);
+
+	printf("%d + %d = %d\n", num1, num2, num1 + num2);
+	printf("%d - %d = %d\n", num1, num2, num1 - num2);
+	printf("%d * %d = %d\n", num1, num2, num1 * num2);
+	printf("%d / %d = %d\n", num1, num2, num1 / num2);
+
+	return 0;
+}
