d268da4aea04df9ae3167cfa47a2b3db67c79fdf
최윤하 <choiyhking@gmail.com>
2022-01-29 15:19:06 +0900

    8-1 해결

---
+++ DS8/8-1.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListBaseStack.h"
+
+int main() {
+	FILE* fp;
+	char op;
+	int num, data;
+
+	Stack stack;
+	StackInit(&stack);
+
+	fp = fopen("input.txt", "r");
+	if (fp == NULL) {
+		printf("FILE OPEN ERROR!\n");
+		exit(1);
+	}
+
+	while (!feof(fp)) {
+		fscanf(fp, "%c ", &op);
+		printf("@@%c@@\n", op);
+
+		if (op == 'I') { // push
+			fscanf(fp, "%d ", &num);
+			SPush(&stack, num);
+			printf("@@%d@@\n", num);
+		}
+		else if(op == 'D') { // 'D': pop
+			data = SPop(&stack);
+		}
+	}
+
+	while (!SIsEmpty(&stack)) {
+		printf("%d ", SPop(&stack));
+	}
+
+	return 0;
+}
